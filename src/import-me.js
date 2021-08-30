26897c8e0d5ae226ed8cf938f958adec747d5a05
jigth <>
2021-08-30 18:08:08 -0500

    Creating Hello World example with Fastify, Babel and Parcel

---
+++ src/import-me.js
@@ -0,0 +1,3 @@
+export function importMe() {
+    console.log("Congratulations, you've imported me :D")
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { importMe } from './import-me.js'; 
+
+importMe()
+
+// Fastify hello world example!
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
